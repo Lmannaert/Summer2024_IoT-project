cd4b7c078d967b70dbbacb3221a3c04521f86271
Lmannaert <114859356+Lmannaert@users.noreply.github.com>
2024-08-11 16:56:22 +0200

    Update keys.py

--- lib/keys.py
+++ lib/keys.py
@@ -1,6 +1,6 @@
-WIFI_SSID = "TN_24GHz_E20C43"
-WIFI_PASS = "LGKLXNETNG"
-TOKEN = "BBUS-2bJX28xtYixzDT9rEAUBcFqXKhbkkg" #Put here your TOKEN
+WIFI_SSID = "Wifi Network"
+WIFI_PASS = "password"
+TOKEN = "Your ubidots API credentials" #Put here your TOKEN
 DEVICE_LABEL = "picowboard" # Assign the device label desire to be send
 VARIABLE_LABEL1 = "temperature"  # Assign the variable label desire to be send
 VARIABLE_LABEL2 = "humidity"
